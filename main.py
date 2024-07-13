e7c0acff2a7a9aa43408faf8e3eca25395828247
arij <karoui.arij20@gamil.com>
2024-07-13 16:08:17 +0100

    add print to main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 my_list = [1,5,7]
-
-def somme(L):
-	s=0
-	for e in L:
-		s+=e
-	return s
-
-my_list = [1,5,7]
-som=somme(my_list)
-print('la somme est =', som)
+print('la somme est =', sum(my_list))
+print('le max est = ', max(my_list))
+print('le min est = ', min(my_list))
